078a78cf6fc7fe0cf5a0079fedf869cd6849be31
Suwaid <suwaidaslam@gmail.com>
2023-01-30 01:57:25 +0500

    backend added

---
+++ client/src/utils/index.js
@@ -0,0 +1,9 @@
+import {surpriseMePrompts} from '../constants';
+
+export function getRandomPromt(prompt){
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+    const randomPromt = surpriseMePrompts[randomIndex];
+
+    if(randomPromt === prompt) return getRandomPromt(prompt);
+    return randomPromt;
+}
